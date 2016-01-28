e8ef1b52a9c017ff89601fa280daac0b7021f2af
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-01-28 08:50:54 +0100

    Passage de l'application en "multiplex" afin d'accélérer la compilation

---
+++ app/src/main/java/com/lifo/cowboy/CowboyApplication.java
@@ -0,0 +1,14 @@
+package com.lifo.cowboy;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Benoît Sauvère on 28/01/16.
+ */
+public class CowboyApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
